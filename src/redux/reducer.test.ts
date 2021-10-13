55c3b833b45c33f11c406667dc09e5d01dc63185
Bhavin789 <bhavib.789@gmail.com>
2021-10-13 23:31:57 +0530

    fix: resolve lint error

--- src/redux/reducer.test.ts
+++ src/redux/reducer.test.ts
@@ -3,31 +3,6 @@ import { reducer } from "./reducer";
 import { AuthState } from "./action";
 
 describe("test reducer", () => {
-  const mockUser = {
-    CreatedDate: "mock-created-date",
-    Email: [
-      {
-        Type: "mock-email-type",
-        Value: "mock-email-value",
-      },
-    ],
-    EmailVerified: true,
-    ID: "mock-id",
-    IsActive: true,
-    IsDeleted: true,
-    LastLoginDate: "mock-login-date",
-    LastLoginLocation: "mock-last-login-action",
-    ModifiedDate: "mock-modified-date",
-    PasswordExpirationDate: "mock-password-expiration",
-    Provider: "mock-provider",
-    RegistrationProvider: "-mock-registration-provider",
-    RegistrationSource: "mock-registration-source",
-    SignupDate: "mock-signup-date",
-    Uid: "mock-uid",
-    Description: "mock-error-description",
-    ErrorCode: 403,
-    Message: "mock-error-message",
-  };
   test("should return isLoading true on LOGIN_POPUP_STARTED action", () => {
     const mockState: AuthState = {
       isAuthenticated: true,
