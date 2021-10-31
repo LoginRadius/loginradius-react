4dd8a73faf81e0647d61d56e4b858e03028ef3e0
AsianCat54x <0xsapphir3@gmail.com>
2021-10-31 21:53:48 +0530

    Add Vuejs Example

---
+++ examples/vue-js-example/src/components/homepage.vue
@@ -0,0 +1,38 @@
+<template>
+    <div id="homepage">
+        <h2>Home Page</h2>
+        <Login v-on:update="onLoggedIn"></Login>
+        <ResendEmailVerification></ResendEmailVerification>
+        <SocialLogin></SocialLogin>
+        <Register></Register>
+    </div>
+</template>
+
+<script>
+    import Login from './login.vue'
+    import ResendEmailVerification from './resend-email.vue'
+    import SocialLogin from './sociallogin.vue'
+    import Register from './register.vue'
+    export default {
+        name: 'homepage',
+        data() {
+            return {
+
+            }
+        },
+        methods: {
+            onLoggedIn() {
+                this.$emit('update',"loggedin")
+            }
+        },
+        components: {
+            Login,
+            ResendEmailVerification,
+            SocialLogin,
+            Register
+        }
+    }
+
+
+
+</script>
---
+++ examples/vue-js-example/src/main.js
@@ -0,0 +1,8 @@
+import Vue from 'vue'
+import App from './App.vue'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './assets/css/main.css'
+new Vue({
+  el: '#app',
+  render: h => h(App)
+})
